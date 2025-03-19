0e9436f74e09c3b11e532979c0f0e6e04160ae04
danipoal <danipoal.0@gmail.com>
2025-03-19 17:15:36 +0100

    Initial commit

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: danalvar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/18 19:38:04 by danalvar          #+#    #+#             */
-/*   Updated: 2025/03/18 19:44:34 by danalvar         ###   ########.fr       */
+/*   Updated: 2025/03/18 21:49:24 by danalvar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,11 @@
 
 # include "libft/libft.h"
 
-
-
-
-
+typedef struct s_node 
+{
+	int	value;
+	struct	s_node	*next;
+} t_node;
 
 
 # endif
--- src/push_swap.c
+++ src/push_swap.c
@@ -6,19 +6,55 @@
 /*   By: danalvar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/18 19:36:02 by danalvar          #+#    #+#             */
-/*   Updated: 2025/03/18 19:57:03 by danalvar         ###   ########.fr       */
+/*   Updated: 2025/03/19 17:15:03 by danalvar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
- #include "../push_swap.h"
+#include "../push_swap.h"
+
+
+int	*parsing(int ac, char **av)
+{
+	int	*numbers;
+
+	numbers = (int *) malloc(ac * sizeof(int));
+	if (!numbers)
+		return (NULL);
+	ac = 2;
+	numbers[0] = ft_atoi(av[ac]);
+	return (numbers);
+}
+
+void	init_stack(t_node **stk, int *nbrs, int size)
+{
+	if (size > 0)
+		(*stk)->value = nbrs[0];
+}
 
 int	main(int argc, char **argv)
 {
+	int	*n;
+	t_node	*stack_a;
+
+	// Handle input
 	if (argc == 2) // Split para eliminar los espacios
 		write(1, *argv, 1);
 	else if (argc > 2)
 	{
 		write(1, "No", 2);
+		n = parsing(argc, argv);
+		if (!n)
+		{
+			write(1, "Error", 5);
+			return (1);
+		}
+		init_stack(&stack_a, n, argc);
+		ft_putnbr_fd(stack_a->value, 1);
 	}
+
+	// Handle errors (suplicates, string, syntaxis, maximums....)
+
+	// Initialize stack a puting the argv and b
+	// Check if is sorted, if not implement algorythm
 	return (0);
 }
